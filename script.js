70f2218f0fe58b55b4d27c31a960669cb535d83d
Jessica Orihuela Rojas <jessica.orihuela.rojas@gmail.com>
2024-08-08 13:44:19 -0400

    Updating countdown

--- script.js
+++ script.js
@@ -85,13 +85,13 @@ document.addEventListener('DOMContentLoaded', () => {
         const distance = countdownDate - now;
 
         // Cálculo de mes, días, horas, minutos y segundos
-        const months = Math.floor(distance / (1000 * 60 * 60 * 24 * 30.42));
+        // const months = Math.floor(distance / (1000 * 60 * 60 * 24 * 30.42));
         const remaining = distance % (1000 * 60 * 60 * 24 * 31);
         const days = Math.floor((remaining / (1000 * 60 * 60 * 24)));
         const hours = Math.floor((remaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const minutes = Math.floor((remaining % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((remaining % (1000 * 60)) / 1000);
-        document.getElementById("months").textContent = formatNumber(months);
+        //document.getElementById("months").textContent = formatNumber(months);
         document.getElementById("days").textContent = formatNumber(days);
         document.getElementById("hours").textContent = formatNumber(hours);
         document.getElementById("minutes").textContent = formatNumber(minutes);
